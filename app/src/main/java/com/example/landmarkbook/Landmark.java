231d4db9a71c5b78f193b29ed2e5ea0be5cd55f5
yenilikci <m.melihcelik017@outlook.com>
2023-03-25 03:18:04 +0300

    added landmark model class

---
+++ app/src/main/java/com/example/landmarkbook/Landmark.java
@@ -0,0 +1,13 @@
+package com.example.landmarkbook;
+
+public class Landmark {
+    String name;
+    String country;
+    int image;
+
+    public Landmark(String name, String country, int image) {
+        this.name = name;
+        this.country = country;
+        this.image = image;
+    }
+}
--- app/src/main/java/com/example/landmarkbook/MainActivity.java
+++ app/src/main/java/com/example/landmarkbook/MainActivity.java
@@ -4,11 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Landmark> landmarkArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        landmarkArrayList = new ArrayList<>();
+
+        Landmark pisa = new Landmark("Pisa", "Italy", R.drawable.pisa);
+        Landmark eiffel = new Landmark("Eiffel", "France", R.drawable.eiffel);
+        Landmark colosseum = new Landmark("Colosseum", "Italy", R.drawable.colosseum);
+        Landmark londonBridge = new Landmark("London Bridge", "UK", R.drawable.londonbridge);
+
+        landmarkArrayList.add(pisa);
+        landmarkArrayList.add(eiffel);
+        landmarkArrayList.add(colosseum);
+        landmarkArrayList.add(londonBridge);
     }
 }
